98b484c18201e314da94492eabbc83ceff000b85
Matheus <matheus.alvfr@gmail.com>
2020-11-17 23:05:35 -0300

    Add Portfolio boilerplate

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
+import { PortfolioModule } from './portfolio/portfolio.module';
 
 @Module({
   imports: [GraphQLModule.forRoot({
     debug: true,
     playground: true,
     autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-  }),],
+  }), PortfolioModule,],
   controllers: [AppController],
   providers: [AppService],
 })
